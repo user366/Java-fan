4c80a3fc83c19505f0ddf0139e238bdcb9dc30ff
user366 <30598279+user366@users.noreply.github.com>
2022-05-24 10:35:08 -0700

    Update fan.java

--- fan.java
+++ fan.java
@@ -1,37 +1,33 @@
-class CeilingFanPullChain {
-    private int currentState;
-
-    public CeilingFanPullChain() {
-        currentState = 0;
+class fanChain {
+    private int velocity;
+    public fanChain() {
+        velocity  = 0;
     }
-
     public void pull() {
-        if (currentState == 0) {
-            currentState = 1;
-            System.out.println("low speed");
-        } else if (currentState == 1) {
-            currentState = 2;
-            System.out.println("medium speed");
-        } else if (currentState == 2) {
-            currentState = 3;
-            System.out.println("high speed");
+        if (velocity == 0) {
+            velocity = 1;
+            System.out.println("speed 1");
+        } else if (velocity== 1) {
+            velocity= 2;
+            System.out.println("speed2");
+        } else if (velocity== 2) {
+            velocity = 3;
+            System.out.println("speed3");
         } else {
-            currentState = 0;
+            velocity= 0;
             System.out.println("turning off");
         }
     }
 }
-
-public class StateDemo {
+public class operatingFan{
     public static void main(String[] args) {
-        CeilingFanPullChain chain = new CeilingFanPullChain();
+        fanChain chain = new fanChain();
         while (true) {
             System.out.print("Press ENTER");
             getLine();
             chain.pull();
         }
     }
-
     static String getLine() {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String line = null;
